ea13fd484a94cb4fc9969bff78262ab71925aba3
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-24 17:19:38 -0800

    port number changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
